b904574246770f3634d664713a5b608c5b1fc72b
BrunoProgramer2 <pap732879@gmail.com>
2023-02-22 19:10:05 -0300

    new version v0.1.3

--- bundler/view.go
+++ bundler/view.go
@@ -1,6 +1,7 @@
 package bundler
 
 func View(dir string, name string) []byte {
+	// bundle
 	bundle := Transform(dir, name)
 
 	return []byte("<div id='root'></div>" + "<script>" + bundle + "</script>")
